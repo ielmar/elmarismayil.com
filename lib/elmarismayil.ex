036076a6a66f5dd6677c63dcf6079a39e1378f2d
Elmar Ismayilov <bamazy@gmail.com>
2023-07-10 00:45:24 +0800

    initial commit

---
+++ lib/elmarismayil.ex
@@ -0,0 +1,18 @@
+defmodule Elmarismayil do
+  @moduledoc """
+  Documentation for `Elmarismayil`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elmarismayil.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
